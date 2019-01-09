8e253bc9d957fc357b808db2266e0c99803ec5d9
Pasindu Gunasekara <pasindugunasekara@hotmail.com>
2019-01-08 22:23:09 -0500

    Added new classes for the two react native pages

---
+++ frontend/mobilityai_react/App/Screens/user_data.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class UserData extends React.Component {
+    
+    render() {
+
+    }
+}
---
+++ frontend/mobilityai_react/App/Screens/user_list.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class UserList extends React.Component {
+    
+    render() {
+
+    }
+}
