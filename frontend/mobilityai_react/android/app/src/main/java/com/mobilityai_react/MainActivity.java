776bbe527a5ac0c75ca054d6761bfb395370dee0
Pasindu Gunasekara <pasindugunasekara@hotmail.com>
2019-01-08 21:38:32 -0500

    Added react native component TODO: Create views for patient selection and individual patient data + integration into native app

---
+++ frontend/mobilityai_react/android/app/src/main/java/com/mobilityai_react/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mobilityai_react;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "mobilityai_react";
+    }
+}
