88e1f2185edde2deb5b6ffa0726311928239ae54
Teodor Voinea <voinea.teodor@gmail.com>
2019-02-04 16:09:32 -0500

    Testing is false

--- backend/mobilityAI/Controllers/MobilityAIController.cs
+++ backend/mobilityAI/Controllers/MobilityAIController.cs
@@ -195,6 +195,7 @@ namespace mobilityAI.Controllers
             form.Add(new StringContent(SERVER_URL + "api/SensorData/MlCallback?Id=" + callbackId), "callback_url");
             form.Add(new ByteArrayContent(accelMs.ToArray()), "file[]", AccelerometerFile.FileName);
             form.Add(new ByteArrayContent(gyroMs.ToArray()), "file[]", GyroscopeFile.FileName);
+            form.Add(new StringContent("false"), "test");
 
             HttpResponseMessage response = await client.PostAsync(ML_SERVER_URL + "windowify", form);
 
