29554b1e39b29773045c6672b0e93b1cafc8b432
Roberto Temelkovski <temelkr@mcmaster.ca>
2019-02-07 22:02:03 -0500

    updated ML server to have up to date experiment code

--- machine_learning/data_processing.py
+++ machine_learning/data_processing.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn import preprocessing
 import numpy as np
 from random import randint
+import math
+from scipy.stats import entropy
 
 def create_training_set(root,files):
     df = pd.read_csv(root+files[0])
@@ -14,90 +16,50 @@ def create_training_set(root,files):
         df = df.append(pd.read_csv(root+files[i]))
     return df
 
-def preprocess_sensor_data(accelerometer_csv, gyroscope_csv):
-    #print("Processing accelerometer")
-    accelerometer_csv = accelerometer_csv.sort_values(by=['epoch (ms)'])
-    accel2 = preprocessing.normalize(accelerometer_csv[['x-axis (g)','y-axis (g)','z-axis (g)']])
-    accelerometer_data = np.concatenate((accelerometer_csv[['epoch (ms)', 'elapsed (s)']], accel2), axis=1)
-    accelerometer_csv = pd.DataFrame(data=accelerometer_data[0:,0:],    # values
-                index=[i for i in range(1, len(accelerometer_data) + 1)],    # 1st column as index
-                columns=['epoch (ms)', 'elapsed (s)', 'x-axis (g)','y-axis (g)','z-axis (g)'])
+def preprocess_accel_data(df):
+    processed = ""
+    df[['x-axis (g)','y-axis (g)','z-axis (g)']] = df[['x-axis (g)','y-axis (g)','z-axis (g)']].apply(pd.to_numeric)
+    testing2 = preprocessing.normalize(df[['x-axis (g)','y-axis (g)','z-axis (g)']])
+    testing_data = np.concatenate((df[['epoch (ms)', 'elapsed (s)']], testing2), axis=1)
+    testing = pd.DataFrame(data=testing_data[0:,0:],    # values
+            index=[i for i in range(1, len(testing_data) + 1)],    # 1st column as index
+            columns=['epoch (ms)', 'elapsed (s)', 'x-axis (g)','y-axis (g)','z-axis (g)'])
+    testing = windowify(2500, 500, testing)
+    p = []
+    for group_name, window in testing.groupby(level=0):
+        p.append(accelerometer_stats(window))
+    processed = pd.DataFrame(p)
+    return processed
 
-    #print("Processing gyro")
-    gyroscope_csv = gyroscope_csv.sort_values(by=['epoch (ms)'])
-    gyro2 = preprocessing.normalize(gyroscope_csv[['x-axis (deg/s)', 'y-axis (deg/s)', 'z-axis (deg/s)']])
-    gyroscope_data = np.concatenate((gyroscope_csv[['epoch (ms)', 'elapsed (s)']], gyro2), axis=1)
-    gyroscope_csv = pd.DataFrame(data=gyroscope_data[0:,0:],    # values
-                index=[i for i in range(1, len(gyroscope_data) + 1)],    # 1st column as index
-                columns=['epoch (ms)', 'elapsed (s)', 'x-axis (deg/s)','y-axis (deg/s)','z-axis (deg/s)'])
+def preprocess_gyro_data(df):
+    processed = ""
+    df[['x-axis (deg/s)', 'y-axis (deg/s)', 'z-axis (deg/s)']] = df[['x-axis (deg/s)', 'y-axis (deg/s)', 'z-axis (deg/s)']].apply(pd.to_numeric)
+    testing2 = preprocessing.normalize(df[['x-axis (deg/s)', 'y-axis (deg/s)', 'z-axis (deg/s)']])
+    testing_data = np.concatenate((df[['epoch (ms)', 'elapsed (s)']], testing2), axis=1)
+    testing = pd.DataFrame(data=testing_data[0:,0:],    # values
+        index=[i for i in range(1, len(testing_data) + 1)],    # 1st column as index
+        columns=['epoch (ms)', 'elapsed (s)', 'x-axis (deg/s)','y-axis (deg/s)','z-axis (deg/s)'])
+    testing = windowify(2500, 500, testing)
+    p = []
+    for group_name, window in testing.groupby(level=0):
+        p.append(gyroscope_stats(window))
+    processed = pd.DataFrame(p)
+    return processed
 
-    #print("Windowifying accelerometer")
-    accelerometer_windows = windowify(2500, 500, accelerometer_csv, accelerometer_stats)
-    accel_df = pd.DataFrame.from_records(accelerometer_windows)
-    
-    #print("Windowifying gyro")
-    gyroscope_windows = windowify(2500, 500, gyroscope_csv, gyroscope_stats)
-    gyro_df = pd.DataFrame.from_records(gyroscope_windows)
-
-    #print("Merging")
-    mergeable_series_lst = build_mergeable_dataframes(accel_df,gyro_df)
-    mergeable_df = pd.DataFrame.from_records(mergeable_series_lst)
-    typecast_map = {"epoch_end":"int64","epoch_start":"int64","epoch_length":"int64"}
-    for i in typecast_map.keys():
-        mergeable_df[i] = mergeable_df[i].astype(typecast_map[i])
-    
-    accel_gyro_df = pd.merge(accel_df,mergeable_df,on=['epoch_start','epoch_end','epoch_length'])
-
-    return accel_gyro_df
-
-def windowify(size, step, dataset, stats_func):
-    # Get the first timestamp
-    start = dataset['epoch (ms)'].iloc[0]
-    
-    #index of the starting timestamp
-    start_index = 0
-    
-    #index of the beginning of the next step
-    step_index = 0
-    
-    #current index
-    i = 0
-    
-    #stores all the windows
-    windows = []
-    
-    #length of the dataset
-    dataset_length = dataset['epoch (ms)'].count()
-    
-    while i < dataset_length:
-        optimal_step = start + step
-        min_step_distance = sys.maxsize
-        #while we haven't reached the end of the dataset and we still have no completed an entire window
-        while i < dataset_length and start + size >= dataset['epoch (ms)'].iloc[i]:
-            #Find the closest entry to where the step _would_ be
-            if abs(optimal_step - dataset['epoch (ms)'].iloc[i]) < min_step_distance:
-                step_index = i
-                min_step_distance = abs(optimal_step - dataset['epoch (ms)'].iloc[i])
-            i += 1
-            
-        #we have a window of duration size that starts at index start_index and ends at i
-        window_frame = dataset[start_index:i]
-        
-        #calculate some information about the window and store it
-        windows.append(stats_func(window_frame))
-        
-        if i == dataset_length:
-            break
-        
-        if dataset['epoch (ms)'].iloc[i] - dataset['epoch (ms)'].iloc[i-1] > size * 2:
-            step_index = i + 1
-
-        #start the next window
-        i = step_index
-        start_index = step_index
-        start = dataset['epoch (ms)'].iloc[i]
-        
-    return windows
+def windows(d, w, t):
+  r = np.arange(len(d))
+  s = r[::t]
+  z = list(zip(s, s + w))
+  f = '{0[0]}:{0[1]}'.format
+  g = lambda t: d.iloc[t[0]:t[1]]
+  return pd.concat(map(g, z), keys=map(f, z))
+  
+def windowify(size, step, dataset):
+  frequency_hz = 1000/25
+  window_size_in_elements = math.ceil(size / frequency_hz)
+  window_step_in_elements = math.ceil(step / frequency_hz)
+  
+  return windows(dataset, window_size_in_elements, window_step_in_elements)
 
 def is_contained_by(absorbing_row,merging_row):
     return absorbing_row['epoch_start'] < merging_row['epoch_start'] and \
@@ -109,7 +71,7 @@ def get_merge_distance(absorbing_row,merging_row):
     return start_dist + end_dist
   
 def merge_rows(absorbing_row,merging_row):
-    return absorbing_row[['epoch_start','epoch_length','epoch_end']].astype('int64').append(merging_row[4:]).to_dict()
+    return absorbing_row[['epoch_start','epoch_length','epoch_end']].astype('int64').append(merging_row[3:]).to_dict()
 
 def build_mergeable_dataframes(df1,df2):
     res = []
@@ -131,6 +93,21 @@ def build_mergeable_dataframes(df1,df2):
                 df1_ptr=nxt_ptr
         df2_ptr += 1
     return res
+
+def performMerge(df1,df2):
+    mergeable_series_lst = build_mergeable_dataframes(df1,df2)
+    mergeable_df = pd.DataFrame.from_records(mergeable_series_lst)
+    typecast_map = {"epoch_end":"int64","epoch_start":"int64","epoch_length":"int64"}
+    for i in typecast_map.keys():
+        mergeable_df[i] = mergeable_df[i].astype(typecast_map[i])
+
+    return pd.merge(df1,mergeable_df,on=['epoch_start','epoch_end','epoch_length'])
+
+def preprocess_sensor_data(accelerometer_csv, gyroscope_csv):
+    accel_df = preprocess_accel_data(accelerometer_csv)
+    gyro_df = preprocess_gyro_data(gyroscope_csv)
+
+    return performMerge(accel_df,gyro_df)
 
 def accelerometer_stats(window_frame):
     return {
