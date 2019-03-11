c20227cfd564e3d11c37e3632dda0167c9371170
Rebecca Tran <tranr5@mcmaster.ca>
2019-03-11 16:49:37 -0400

    added back removed line

--- frontend/mobilityai_react/App/Lib/Api.js
+++ frontend/mobilityai_react/App/Lib/Api.js
@@ -14,8 +14,8 @@ export function AddPatientData(patientData) {
         .catch((error) => console.log(JSON.stringify(error)));
 };
 
-
-    const route = "api/Patients/";
+export function GetPatients() {
+    const route = "api/Patients";
     let url = encodeURI(SERVER_URL + route);
     console.log(SERVER_URL);
     console.log("GetPatients: Make get request to: " + url);
