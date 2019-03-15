7f6fb07dd815616a3262a46b9b0e8f9f8b3b0007
Pasindu Gunasekara <pasindugunasekara@hotmail.com>
2019-03-15 14:13:23 -0400

    Added new buttons for manually starting and stopping the device

--- frontend/mobilityai/app/src/main/java/com/ai/mobility/mobilityai/DeviceInfoActivity.java
+++ frontend/mobilityai/app/src/main/java/com/ai/mobility/mobilityai/DeviceInfoActivity.java
@@ -8,18 +8,13 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.IBinder;
-import android.renderscript.ScriptGroup;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,41 +24,15 @@ import android.widget.Toast;
 
 import com.android.volley.Response;
 import com.android.volley.error.VolleyError;
-import com.mbientlab.metawear.Data;
-import com.mbientlab.metawear.MetaWearBoard;
-import com.mbientlab.metawear.Route;
-import com.mbientlab.metawear.Subscriber;
 import com.mbientlab.metawear.android.BtleService;
-import com.mbientlab.metawear.builder.RouteComponent;
-import com.mbientlab.metawear.data.Acceleration;
-import com.mbientlab.metawear.data.AngularVelocity;
-import com.mbientlab.metawear.module.Accelerometer;
-import com.mbientlab.metawear.module.GyroBmi160;
-import com.mbientlab.metawear.module.Led;
-import com.mbientlab.metawear.module.Logging;
-import com.mbientlab.metawear.module.Settings;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import bolts.Continuation;
 import bolts.Task;
 
 public class DeviceInfoActivity extends AppCompatActivity implements ServiceConnection {
     private final String TAG = "MobilityAI";
 
     private TextView m_batteryPercentage, m_devName, m_macAddr, m_lastSync, m_batteryText;
-    private Button m_syncButton, m_reassignButton, m_startLoggingButton, m_stopLoggingButton;
+    private Button m_startButton, m_stopButton, m_reassignButton;
     private ProgressBar m_batteryCircle, m_syncProgressBar, m_loadingBar;
 
     private MetaMotionService m_service;
@@ -114,7 +83,8 @@ public class DeviceInfoActivity extends AppCompatActivity implements ServiceConn
         m_macAddr.setVisibility(View.INVISIBLE);
         m_lastSync.setVisibility(View.INVISIBLE);
         m_batteryText.setVisibility(View.INVISIBLE);
-        m_syncButton.setVisibility(View.INVISIBLE);
+        m_startButton.setVisibility(View.INVISIBLE);
+        m_stopButton.setVisibility(View.INVISIBLE);
         m_reassignButton.setVisibility(View.INVISIBLE);
         m_batteryCircle.setVisibility(View.INVISIBLE);
         m_syncProgressBar.setVisibility(View.INVISIBLE);
@@ -129,7 +99,8 @@ public class DeviceInfoActivity extends AppCompatActivity implements ServiceConn
         m_macAddr.setVisibility(View.VISIBLE);
         m_lastSync.setVisibility(View.VISIBLE);
         m_batteryText.setVisibility(View.VISIBLE);
-        m_syncButton.setVisibility(View.VISIBLE);
+        m_startButton.setVisibility(View.VISIBLE);
+        m_stopButton.setVisibility(View.VISIBLE);
         m_reassignButton.setVisibility(View.VISIBLE);
         m_batteryCircle.setVisibility(View.VISIBLE);
         m_syncProgressBar.setVisibility(View.VISIBLE);
@@ -217,12 +188,11 @@ public class DeviceInfoActivity extends AppCompatActivity implements ServiceConn
         m_lastSync = findViewById(R.id.devLastSync);
         m_batteryCircle = findViewById(R.id.progressBar);
         m_syncProgressBar = findViewById(R.id.syncProgress);
-        m_syncButton = findViewById(R.id.syncButton);
+        m_startButton = findViewById(R.id.startButton);
+        m_stopButton = findViewById(R.id.stopButton);
         m_reassignButton = findViewById(R.id.reassignButton);
         m_batteryText = findViewById(R.id.batteryIdentifier);
         m_loadingBar = findViewById(R.id.loadingCircle);
-        m_startLoggingButton = findViewById(R.id.startLoggingButton);
-        m_stopLoggingButton = findViewById(R.id.stopLoggingButton);
 
         m_reassignButton.setOnClickListener(l -> {
 
@@ -234,7 +204,7 @@ public class DeviceInfoActivity extends AppCompatActivity implements ServiceConn
      * Sync button handler to connect, sync data, and restart logging
      */
     private void setSyncButtonOnClickHandler() {
-        m_syncButton.setOnClickListener((View v) -> {
+        m_startButton.setOnClickListener((View v) -> {
             //TODO: Implement in next commit
         });
     }
