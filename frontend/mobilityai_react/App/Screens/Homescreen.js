0d0567f311f551b022fe10f9429909bdabe1f4cd
Roberto Temelkovski <temelkr@mcmaster.ca>
2019-03-27 15:05:37 -0400

    refactor homescreen (extract some copy paste into it's own component

--- frontend/mobilityai_react/App/Screens/Homescreen.js
+++ frontend/mobilityai_react/App/Screens/Homescreen.js
@@ -4,48 +4,27 @@ import { TouchableHighlight, Button, StyleSheet, Text, View, Image } from 'react
 export default class Homescreen extends Component {
   render() {
     const { navigate } = this.props.navigation;
+    const HomePageButton = (props) => <Button {...props} style={styles.button} color="#5DACBD"/>
 
     return (
-      <View style={styles.container1}>
-        {/* Initial screen displaying logo and buttons for users to either Sign up or Sign in */}
-        <View style={styles.container2}>
+      <View style={styles.pageContainer}>
+        <View style={styles.logoContainer}>
           <Image
             style={styles.img}
             source={{ uri: 'https://raw.githubusercontent.com/pgunasekara/4zp6/master/media/logo_transparent.png' }}
           />
         </View>
 
-        {/* TEMP */}
-        {/* <View>
-          <TouchableHighlight onPress={() => navigate('PatientObservations', {
-            id: 0,
-            firstName: 'Rebecca',
-            lastName: 'Tran',
-            navigation: this.props.navigation
-          })}>
-            <View>
-              
-            </View>
-          </TouchableHighlight>
-        </View> */}
-
-        {/* Button redirecting users to Sign Up page */}
-        <View style={styles.buttons}>
-          <Button
-            style={styles.mainButton}
+        <View style={styles.buttonContainer}>
+          <HomePageButton
             title='Sign Up'
             onPress={() => navigate('SignUp', {})}
-            color="#5DACBD"
           />
 
-          {/* Button redirecting users to Sign in page */}
-          <Button
-            style={styles.mainButton}
+          <HomePageButton
             title='Sign In'
             onPress={() => navigate('SignIn', {})}
-            color="#5DACBD"
           />
-
         </View>
       </View>
     );
@@ -53,13 +32,12 @@ export default class Homescreen extends Component {
 }
 
 const styles = StyleSheet.create({
-  container1: {
+  pageContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'stretch',
   },
-
-  container2: {
+  logoContainer: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
@@ -70,8 +48,7 @@ const styles = StyleSheet.create({
     width: 350,
     height: 350,
   },
-
-  buttons: {
+  buttonContainer: {
     flexDirection: 'column',
     justifyContent: 'space-between',
     height: 225,
@@ -79,7 +56,7 @@ const styles = StyleSheet.create({
     width: 250,
     alignSelf: "center"
   },
-  mainButton: {
+  button: {
     borderColor : "#5DACBD",
   },
 });
