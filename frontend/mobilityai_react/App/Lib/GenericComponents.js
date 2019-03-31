6d2944a30a8da33ae1213dddf909c841f45d3168
Roberto Temelkovski <temelkr@mcmaster.ca>
2019-03-31 18:31:01 -0400

    removed unused component

--- frontend/mobilityai_react/App/Lib/GenericComponents.js
+++ frontend/mobilityai_react/App/Lib/GenericComponents.js
@@ -2,16 +2,3 @@ import React from 'react';
 import { TouchableOpacity, Button, ScrollView, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export const LoadingComponent = ({message}) => <View><Text>{message}</Text></View>;
-
-export const TitledTextInput = ({title, compStyle, ...props}) => {
-	return (
-		<View>
-            <Text>{title}</Text>
-            <TextInput
-                style={compStyle}
-                placeholder={title}
-                {...props}
-            />
-		</View>
-	)
-}
