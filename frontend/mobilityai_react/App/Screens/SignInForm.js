295d9242ee878fa978408e4fb95764ad6a852a7d
Roberto Temelkovski <temelkr@mcmaster.ca>
2019-04-01 16:04:59 -0400

    added in thing that fixed error

--- frontend/mobilityai_react/App/Screens/SignInForm.js
+++ frontend/mobilityai_react/App/Screens/SignInForm.js
@@ -5,6 +5,7 @@ import { Input, Icon } from 'react-native-elements'
 export default class SignIn extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {};
     }
 
     /*
